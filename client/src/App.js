cfe091371d8e5da09e8067ac97d60f57023863d2
witoldmetel <witoldmetel@gmail.com>
2018-06-29 19:57:27 +0200

    Add Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavbar from './components/AppNavbar';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Test
+        <AppNavbar />
       </div>
     );
   }
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+
+export default class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    };
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+        <div>
+            <Navbar color="dark" dark expand="sm">
+                <NavbarBrand href="/">Todo App</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="https://github.com/witoldmetel/MERN_Interview" target="_blank">
+                            GitHub
+                        </NavLink>
+                    </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+        )
+    }
+}
