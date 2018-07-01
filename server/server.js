9c61efc7b094cd8324e7b9f7e873a0a0f2a1badc
witoldmetel <witoldmetel@gmail.com>
2018-07-01 16:46:31 +0200

    add heroku-cli

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const todos = require('./schema/todos');
 
@@ -19,6 +20,15 @@ mongoose.connect(db)
 
 // Use API Route (endpoint, refs)
 app.use('/todos', todos);
+
+//Serve static assets for production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('../client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 // Port Server define
 const port = process.env.PORT || 3030;
